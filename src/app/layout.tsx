6bda476e95ba98f3fe67aa046b434640ee884f31
Rahul Shetty <shettyrahul8june@gmail.com>
2024-04-14 22:31:53 +0530

    feat: configure linting, add favicon and format code

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,12 +1,47 @@
-import type { Metadata } from "next";
-import { Inter } from "next/font/google";
-import "./globals.css";
+import './globals.css';
 
-const inter = Inter({ subsets: ["latin"] });
+import type { Metadata } from 'next';
+import { Inter } from 'next/font/google';
+
+const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
-  title: "Lottify",
-  description: "Lightweight, scalable animations for your websites and apps",
+  title: 'Lottify',
+  description: 'Lightweight, scalable animations for your websites and apps',
+  icons: [
+    {
+      href: 'favicon/favicon.ico',
+      url: 'favicon/favicon.ico',
+    },
+    // apple touch icons
+    {
+      href: 'favicon/apple-touch-icon.png',
+      url: 'favicon/apple-touch-icon.png',
+    },
+    // android chrome icons
+    {
+      href: 'favicon/android-chrome-192x192.png',
+      url: 'favicon/android-chrome-192x192.png',
+      sizes: '192x192',
+    },
+    {
+      href: 'favicon/android-chrome-512x512.png',
+      url: 'favicon/android-chrome-512x512.png',
+      sizes: '512x512',
+    },
+    // 16x16 icon
+    {
+      href: 'favicon/favicon-16x16.png',
+      url: 'favicon/favicon-16x16.png',
+      sizes: '16x16',
+    },
+    // 32x32 icon
+    {
+      href: 'favicon/favicon-32x32.png',
+      url: 'favicon/favicon-32x32.png',
+      sizes: '32x32',
+    },
+  ],
 };
 
 export default function RootLayout({
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,8 @@
-import Image from "next/image";
-
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      Lottify UI
+      <h1 className="text-3xl font-bold">Lottify UI</h1>
+      <p className="text-center text-lg">Welcome to the Next.js + Tailwind CSS Starter</p>
     </main>
   );
 }
