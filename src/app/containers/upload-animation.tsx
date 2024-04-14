44ad179fffb0acfefd2e7678dcea5511ff065f6c
Rahul Shetty <shettyrahul8june@gmail.com>
2024-04-15 03:50:10 +0530

    feat: allow users to upload files using react-dropzone

--- src/app/containers/upload-animation.tsx
+++ src/app/containers/upload-animation.tsx
@@ -1,6 +1,7 @@
 'use client';
 import { CloudArrowUpIcon } from '@heroicons/react/16/solid';
 import { DocumentArrowUpIcon } from '@heroicons/react/24/solid';
+import { useDropzone } from 'react-dropzone';
 
 import Button from '@/components/core/button';
 import { Modal } from '@/components/core/modal';
@@ -8,6 +9,12 @@ import useToggle from '@/hooks/useToggle';
 
 export const UploadAnimation = () => {
   const [isOpen, toggle] = useToggle();
+  const onDrop = (acceptedFiles: File[]) => {
+    // Handle file upload logic here
+    console.info('Accepted files:', acceptedFiles);
+  };
+
+  const { getRootProps, getInputProps } = useDropzone({ onDrop });
 
   return (
     <>
@@ -21,21 +28,26 @@ export const UploadAnimation = () => {
       </Button>
 
       <Modal title="Upload animation" isOpen={isOpen} toggle={toggle}>
-        <div className="my-6 space-y-5 rounded-lg border-2 border-dashed border-gray-100 py-6">
-          <DocumentArrowUpIcon className="mx-auto h-16 w-16 text-gray-300" />
+        <div {...getRootProps({ className: 'dropzone' })}>
+          <div className="my-6 space-y-5 rounded-lg border-2 border-dashed border-gray-100 py-6">
+            <input {...getInputProps()} />
+            <DocumentArrowUpIcon className="mx-auto h-16 w-16 text-gray-300" />
 
-          <div className="text-center">
-            <p className="font-lf-bold text-base text-gray-300">Lottie JSON</p>
-            <p className="text-xs text-gray-300">Max file size: 20 MB each</p>
-          </div>
+            <div className="text-center">
+              <p className="font-lf-bold text-base text-gray-300">
+                Lottie JSON
+              </p>
+              <p className="text-xs text-gray-300">Max file size: 20 MB each</p>
+            </div>
 
-          <div className="flex flex-col items-center">
-            <p className="text-center text-xl font-bold text-gray-600">
-              Drag &amp; drop animations to upload
-            </p>
-            <span className="mb-2 text-gray-600">or</span>
+            <div className="flex flex-col items-center">
+              <p className="text-center text-xl font-bold text-gray-600">
+                Drag &amp; drop animations to upload
+              </p>
+              <span className="mb-2 text-gray-600">or</span>
 
-            <Button type="button">Browse</Button>
+              <Button type="button">Browse</Button>
+            </div>
           </div>
         </div>
       </Modal>
